a8fd050b3442fc110480d97de6fe7cc2b9a08fcd
bea09 <bea09bea@gmail.com>
2026-09-01 13:33:39 +0200

    created register and homepage activity

--- app/src/main/java/com/bea/lifecyclev6/MainActivity.java
+++ app/src/main/java/com/bea/lifecyclev6/MainActivity.java
@@ -1,7 +1,12 @@
 package com.bea.lifecyclev6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +22,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //Log in btn -> Homepage activity
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomepageActivity.class);
+                startActivity(i);
+            }
+        });
 
+        //Register link -> register activity
+        TextView link = findViewById(R.id.registerLink);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
