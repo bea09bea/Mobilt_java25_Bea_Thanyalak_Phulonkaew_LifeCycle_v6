19c73207e94ffc39532bdc30a0808fc87922b615
bea09 <bea09bea@gmail.com>
2026-09-03 21:48:52 +0200

    done

--- app/src/main/java/com/bea/lifecyclev6/HomepageActivity.kt
+++ app/src/main/java/com/bea/lifecyclev6/HomepageActivity.kt
@@ -1,24 +1,171 @@
-package com.bea.lifecyclev6;
+package com.bea.lifecyclev6
 
-import android.os.Bundle;
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.sqrt
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+class HomepageActivity : AppCompatActivity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
 
-public class HomepageActivity extends AppCompatActivity {
+    //Step counter
+    private var stepcounter: Sensor? = null
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    //Accelerometer som backup
+    private var accelerometer: Sensor? = null
+
+    private var usingStepCounter = false
+
+    //används när accelerometer backup
+    private var steps = 0
+    private var lastMagnitude = 0f
+    private var stepText: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_homepage)
+
+        stepText = findViewById<TextView>(R.id.steps)
+
+        //försök använda step counter
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        stepcounter = sensorManager!!.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        //hämta accelerometern som backup
+        accelerometer = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        //kontrollera om step countern finns
+        if (stepcounter != null) {
+            usingStepCounter = true
+            stepText!!.setText("Your steps: 0")
+        } else {
+            //step counter finns inte
+            usingStepCounter = false
+
+            if (accelerometer != null) {
+                stepText!!.setText(
+                    "Step Counter not supported.\n" +
+                            "Using Accelerometer instead.\n\n" +
+                            "Steps: 0"
+                )
+            } else {
+                stepText!!.setText(
+                    "Step Counter and Accelerometer " +
+                            "are not supported on this device."
+                )
+            }
+        }
+
+        //byt till räkna bmi vy
+        val btn = findViewById<Button>(R.id.countBMIbtn)
+        btn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val i = Intent(this@HomepageActivity, BmiActivity::class.java)
+                startActivity(i)
+            }
+        })
+
+        val profileText = findViewById<TextView>(R.id.profile);
+
+        //hämtar data från sharedPreferences
+        val preferences = getSharedPreferences("FitnessApp", MODE_PRIVATE);
+        val name = preferences.getString("name", "");
+        val gender = preferences.getString("gender", "");
+        val phone = preferences.getString("phonenumber", "");
+        val birthday = preferences.getLong("birthday",0L);
+        val weight = preferences.getFloat("weight", 0f);
+        val height = preferences.getFloat("height", 0f);
+        val bmi = preferences.getFloat("bmi", 0f);
+
+        //formaterar timestamp till yyyy-MM-dd
+        val formatter = java.text.SimpleDateFormat("yyyy-MM-dd", java.util.Locale.getDefault())
+        val birthdaytext = formatter.format(java.util.Date(birthday));
+
+        profileText.text =
+            "Name: $name\n\n" +
+            "Gender: $gender\n\n" +
+            "Phonenumber: $phone\n\n"+
+            "Birthday: $birthdaytext\n\n" +
+            "Weigth: $weight kg\n\n" +
+            "Height: $height cm\n\n" +
+            "BMI: $bmi"
+
+        //logga ut
+        val logout = findViewById<Button>(R.id.logout)
+        logout.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val i = Intent(this@HomepageActivity, MainActivity::class.java)
+                startActivity(i)
+            }
+        })
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById<View?>(R.id.main),
+            OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat? ->
+                val systemBars = insets!!.getInsets(WindowInsetsCompat.Type.systemBars())
+                v!!.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //stäng av sensorn när activity inte längre är aktiv
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (usingStepCounter && stepcounter != null) {
+            //aktivera step counter
+            sensorManager!!.registerListener(this, stepcounter, SensorManager.SENSOR_DELAY_NORMAL)
+        } else if (accelerometer != null) {
+            //backup: aktivera accelerometer
+            sensorManager!!.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        //step counter
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            val totalSteps = event.values[0].toInt()
+            stepText!!.setText("Steps: " + totalSteps)
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            //beräkna rörelse storlek
+            val magnitude = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+
+            //stegräkning
+            if (magnitude - lastMagnitude > 2.0f) {
+                steps++
+
+                stepText!!.setText(
+                    "Step Counter not supported.\n" +
+                            "Using Accelerometer.\n\n" +
+                            "Steps: " + steps
+                )
+            }
+
+            lastMagnitude = magnitude
+        }
     }
 }
--- app/src/main/java/com/bea/lifecyclev6/MainActivity.java
+++ app/src/main/java/com/bea/lifecyclev6/MainActivity.java
@@ -2,11 +2,12 @@ package com.bea.lifecyclev6;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,19 +17,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String correctMail = "test@gmail.com";
+    private final String correctPassword = "123";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //Log in btn -> Homepage activity
+        EditText mailInput = findViewById(R.id.mailInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
         Button btn = findViewById(R.id.button);
+
+        //Log in btn -> Homepage activity
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, HomepageActivity.class);
-                startActivity(i);
+
+                //Inloggning
+                String email = mailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString();
+
+                if (email.equals(correctMail) && password.equals(correctPassword)) {
+                    Intent i = new Intent(MainActivity.this, HomepageActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(MainActivity.this, "Wrong mail or password", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -48,17 +65,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("BEA","onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("BEA","onStop");
     }
 }
